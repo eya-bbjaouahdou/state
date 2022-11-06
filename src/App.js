e2124ef02550aa76be7f629a1f48a5d33780dcc0
eya-bbjaouahdou <eyajaouahdou78@gmail.com>
2022-11-06 10:56:42 +0100

    first state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState , useEffect} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [ timer , setTimer ] = useState(0) ;
+ 
+  useEffect(() => {
+   setInterval(() => {
+     setTimer(timer => timer + 1); 
+   }, 1000)
+  }, [])
+  
+
+
+  return(
+    <div  className="Container my-5"> 
+     <div className="card text-center my-5">
+      <div className="card-body">
+        <h1>Chrono With React With State </h1>
+        <div className="my-5">
+          <h2 className="my-5">{timer}</h2>
+          <button className="btn btn-success mx-3" 
+          onClick={() => setTimer(timer + 1)}
+          disabled = {timer === 0}
+          >Increment</button>
+
+          
+          
+          <button className="btn btn-danger mx-3" 
+          onClick={() => setTimer(timer - 1)}
+          disabled = {timer === 0}>
+            Decrement</button>
+          
+          
+          
+          <button className="btn btn-secondary mx-3"
+           onClick={() => setTimer(0)}
+           disabled = {timer === 0}>
+            Reset</button>
+        </div>
+      </div>
+     </div>
+    
     </div>
+
   );
 }
 
